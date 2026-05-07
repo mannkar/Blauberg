cca397590b0052106db3e1ced59400a6e9fdb655
mannkar <forum@hofmann-kontakt.de>
2026-05-07 09:12:30 +0200

    Init

---
+++ VentoExpert/module.php
@@ -0,0 +1,163 @@
+<?php
+
+declare(strict_types=1);
+
+// CLASS VentoExpert
+class VentoExpert extends IPSModuleStrict
+{
+
+    /**
+     * In contrast to Construct, this function is called only once when creating the instance and starting IP-Symcon.
+     * Therefore, status variables and module properties which the module requires permanently should be created here.
+     *
+     * @return void
+     */
+    public function Create(): void
+    {
+        //Never delete this line!
+        parent::Create();
+
+        // Set visualization type to 1, as we want to offer HTML
+        $this->SetVisualizationType(1);
+    }
+
+    /**
+     * This function is called when deleting the instance during operation and when updating via "Module Control".
+     * The function is not called when exiting IP-Symcon.
+     *
+     * @return void
+     */
+    public function Destroy(): void
+    {
+        parent::Destroy();
+    }
+
+    /**
+     * The content can be overwritten in order to transfer a self-created configuration page.
+     * This way, content can be generated dynamically.
+     * In this case, the "form.json" on the file system is completely ignored.
+     *
+     * @return string Content of the configuration page.
+     */
+    public function GetConfigurationForm(): string
+    {
+        // Get Form
+        $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+
+        // Debug output
+        $this->SendDebug(__FUNCTION__, $form, 0);
+        return json_encode($form);
+    }
+
+    /**
+     * Is executed when "Apply" is pressed on the configuration page and immediately after the instance has been created.
+     *
+     * @return void
+     */
+    public function ApplyChanges(): void
+    {
+        parent::ApplyChanges();
+
+        // Set status
+        $this->SetStatus(102);
+    }
+
+    /**
+     * The content of the function can be overwritten in order to carry out own reactions to certain messages.
+     * The function is only called for registered MessageIDs/SenderIDs combinations.
+     *
+     * data[0] = new value
+     * data[1] = value changed?
+     * data[2] = old value
+     * data[3] = timestamp.
+     *
+     * @param int   $timestamp Continuous counter timestamp
+     * @param int   $sender    Sender ID
+     * @param int   $message   ID of the message
+     * @param array{0:mixed,1:bool,2:mixed,3:int} $data Data of the message
+     *
+     * @return void
+     */
+    public function MessageSink(int $timestamp, int $sender, int $message, array $data): void
+    {
+        // check if state really changed ?
+        if ($data[1] != true) {
+            return;
+        }
+
+        if ($message === VM_UPDATE) {
+            $this->SendDebug(__FUNCTION__, "Update of $sender = $data[0]", 0);
+            // TODO: Add your programming logic here
+        }
+    }
+
+    /**
+     * Is called when, for example, a button is clicked in the visualization.
+     *
+     * @param string $ident Ident of the variable
+     * @param mixed $value The value to be set
+     *
+     * @return void
+     */
+    public function RequestAction(string $ident, mixed $value): void
+    {
+        // Debug output
+        $this->SendDebug(__FUNCTION__, $ident . ' => ' . $value, 0);
+        // TODO: Replace identifier
+        switch ($ident) {
+            case 'OnXxxxxYyyyy':
+                break;
+            default:
+                $this->SendDebug(__FUNCTION__, 'There was no reaction to the action.', 0);
+        }
+        // Send a complete update message to the display, as parameters may have changed
+        // $this->UpdateVisualizationValue($this->GetFullUpdateMessage());
+        return;
+    }
+
+    /**
+     * If the HTML-SDK is to be used, this function must be overwritten in order to return the HTML content.
+     *
+     * @return string Initial display of a representation via HTML SDK
+     */
+    public function GetVisualizationTile(): string
+    {
+        // Add a script to set the values when loading, analogous to changes at runtime
+        // Although the return from GetFullUpdateMessage is already JSON-encoded, json_encode is still executed a second time
+        // This adds quotation marks to the string and any quotation marks within it are escaped correctly
+        $handling = '<script>handleMessage(' . json_encode($this->GetFullUpdateMessage()) . ');</script>';
+        // Add static HTML from file
+        $module = file_get_contents(__DIR__ . '/module.html');
+        // Important: $initialHandling at the end, as the handleMessage function is only defined in the HTML
+        return $module . $handling;
+    }
+
+    /**
+     * Generate a message that updates all elements in the HTML display.
+     *
+     * @return string JSON encoded message information
+     */
+    private function GetFullUpdateMessage(): string
+    {
+        // Fill resultset
+        $result = [];
+        $result['text'] = "Here's the text!";
+        $result['bgcolor'] = $this->ReadPropertyInteger('PriceFont');
+        $this->SendDebug(__FUNCTION__, print_r($result, true), 0);
+        // send it
+        return json_encode($result);
+    }
+
+   /**
+     * Show message via popup
+     *
+     * @param string $caption echo message
+     *
+     * @return void
+     */
+    private function EchoMessage(string $caption): void
+    {
+        $this->UpdateFormField('EchoMessage', 'caption', $this->Translate($caption));
+        $this->UpdateFormField('EchoPopup', 'visible', true);
+    }
+}
